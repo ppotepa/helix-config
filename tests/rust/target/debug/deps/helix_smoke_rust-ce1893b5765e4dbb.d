ddc591efc6723bcbb99871210ce2a452e83f1c04
Paweł Potępa <pawel.potepa@hotmail.com>
2026-03-12 11:56:11 +0100

    Remove Rust build artifacts from test workspace

--- tests/rust/target/debug/deps/helix_smoke_rust-ce1893b5765e4dbb.d
+++
@@ -1,5 +0,0 @@
-/home/ppotepa/git/helix-config/tests/rust/target/debug/deps/helix_smoke_rust-ce1893b5765e4dbb.d: src/main.rs
-
-/home/ppotepa/git/helix-config/tests/rust/target/debug/deps/libhelix_smoke_rust-ce1893b5765e4dbb.rmeta: src/main.rs
-
-src/main.rs:
